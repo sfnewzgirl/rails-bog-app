84fbf6bbac5b4116ba0479152dea3ee88018f519
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-30 13:59:12 -0700

    working edit/update, redirect bug

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -11,9 +11,20 @@ class CreaturesController < ApplicationController
   end
   def create
     creature_params = params.require(:creature).permit(:name, :description)
-    creature = Creature.new(creature_params)
-    if creature.save
-      redirect_to creatures_path(creature)
+    @creature = Creature.new(creature_params)
+    if @creature.save
+      redirect_to creatures_path(@creature)
     end
+  end
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+  end
+  def update
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    creature_params = params.require(:creature).permit(:name, :description)
+    @creature.update_attributes(creature_params)
+    redirect_to creatures_path(@creature)
   end
 end
