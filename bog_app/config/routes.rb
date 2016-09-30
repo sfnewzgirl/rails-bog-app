64e8bb0c05e7b5c224b7666827d33f704bc58f0b
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-30 13:17:13 -0700

    creates post function for new form

--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "creatures#index"
 
   get "/creatures", to: "creatures#index", as: "creatures"
-
-  get "/creatures/new", to: "creatures#new", as: "new_creatures"
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "/creatures", to: "creatures#create"
 
 end
