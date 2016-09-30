b3aa335f80c00524b66c7402b5b9617ef9787c6d
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-09-30 14:43:56 -0700

    fixes delete bug

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -13,7 +13,7 @@ class CreaturesController < ApplicationController
     creature_params = params.require(:creature).permit(:name, :description)
     @creature = Creature.new(creature_params)
     if @creature.save
-      redirect_to creatures_path(@creature)
+      redirect_to creature_path(@creature)
     end
   end
   def edit
@@ -25,7 +25,7 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
     creature_params = params.require(:creature).permit(:name, :description)
     @creature.update_attributes(creature_params)
-    redirect_to creatures_path(@creature)
+    redirect_to creature_path(@creature)
   end
   def destroy
     creature_id = params[:id]
