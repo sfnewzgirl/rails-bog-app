9e5a94328df2d5a26b7e98403bc52547014cff37
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-02 13:17:07 -0700

    delete

--- bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -27,4 +27,10 @@ class CreaturesController < ApplicationController
     creature.update_attributes(creature_params)
     redirect_to creature_path(creature)
   end
+  def destroy
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+  end
 end
