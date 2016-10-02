adbccf85c73f3babf98bc8f66b4e1b5e92579a57
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-02 12:45:43 -0700

    creates new submit method, refactors view

--- bog_app/bog_app/bog_app/config/routes.rb
+++ bog_app/bog_app/bog_app/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   get "/creatures", to: "creatures#index", as: "creatures"
   get "/creatures/new", to: "creatures#new", as: "new_creature"
+  post "/creatures", to: "creatures#create", as: "create_creature"
 end
