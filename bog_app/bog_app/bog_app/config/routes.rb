f04c5ad0dd6e980d4c5b6268d4b2d2f9ff44eff7
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-02 12:33:58 -0700

    creates new creature form

--- bog_app/bog_app/bog_app/config/routes.rb
+++ bog_app/bog_app/bog_app/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "creatures#index"
 
   get "/creatures", to: "creatures#index", as: "creatures"
+  get "/creatures/new", to: "creatures#new", as: "new_creature"
 end
