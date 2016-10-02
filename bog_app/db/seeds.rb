b17213b932232321809121fec96ba2db4f539c92
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-02 14:24:29 -0700

    initial commit with index

---
+++ bog_app/db/seeds.rb
@@ -0,0 +1,3 @@
+Creature.create({name: "Luke Skywalke", description: "Jedi"})
+Creature.create({name: "Darth Vader", description: "Luke's father"})
+Creature.create({name: "Rodents of Unusual Size", description: "Agressive rat-like beasts"})
