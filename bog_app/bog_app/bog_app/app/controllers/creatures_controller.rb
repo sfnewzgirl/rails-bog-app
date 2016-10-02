e719e49ac870a0313b507149692d8381b136d5e2
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-02 13:12:59 -0700

    edit and update methods, routes

--- bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
+++ bog_app/bog_app/bog_app/app/controllers/creatures_controller.rb
@@ -16,4 +16,15 @@ class CreaturesController < ApplicationController
     creature_params = params[:id]
     @creature = Creature.find_by_id(creature_params)
   end
+  def edit
+    creature_params = params[:id]
+    @creature = Creature.find_by_id(creature_params)
+  end
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
 end
