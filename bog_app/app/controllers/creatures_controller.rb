479f84370855e526e97799ce79ae2c05883da56f
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-02 14:34:59 -0700

    creates new creature

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -3,5 +3,13 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
   end
   def new
+    @creature = Creature.new
+  end
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.new(creature_params)
+    if creature.save
+      redirect_to creatures_path
+    end
   end
 end
