9e86a9d54bd3c4bdfec5b9a2f76f85d717b1216d
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-02 14:39:20 -0700

    new, create, show working

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -9,7 +9,11 @@ class CreaturesController < ApplicationController
     creature_params = params.require(:creature).permit(:name, :description)
     creature = Creature.new(creature_params)
     if creature.save
-      redirect_to creatures_path
+      redirect_to creature_path(creature)
     end
+  end
+  def show
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
   end
 end
